8c3d42d9215334c2fbfa516a605d4f888b75dd24
Neel Smith <dnsmith.neel@gmail.com>
2016-03-10 11:13:20 -0500

    Make some changes

--- scripts/parseFile.groovy
+++ scripts/parseFile.groovy
@@ -1,4 +1,12 @@
+// Both transcoder and greekutils available from beta.hpcc.uh.edu:
+@GrabResolver(name='beta', root='http://beta.hpcc.uh.edu/nexus/content/repositories/releases')
+@Grab(group='edu.harvard.chs', module='cite', version="1.3.0")
+@Grab(group='edu.holycross.shot', module='morphology', version="0.2.0")
+@Grab(group='edu.holycross.shot', module='common', version="2.3.1")
+
+import edu.harvard.chs.cite.CiteUrn
 import edu.holycross.shot.greekmorph.UrnManager
+
 
 if (args.size() != 1) {
   System.err.println "Usage: groovy parseFile.groovy <FILENAME>"
@@ -13,3 +21,9 @@ println "Parse file " + f
 File urnRegistry = new File("/vagrant/greeklang/morphology/data/greek/urnregistry/collectionregistry.csv")
 UrnManager umgr = new UrnManager(urnRegistry)
 println "Made urn manager"
+
+
+
+CiteUrn urn = new CiteUrn("urn:cite:hmt:msA.12r")
+
+println "resolved!"
