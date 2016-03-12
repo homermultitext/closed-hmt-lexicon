4cd548dc2fc6682d055c644c3cb1b1a99e7f77b6
Neel Smith <dnsmith.neel@gmail.com>
2016-03-12 07:30:23 -0500

    Correct property name

--- buildFst.sh
+++ buildFst.sh
@@ -4,7 +4,7 @@ GRADLE=`which gradle`
 
 cd /vagrant/greeklang/morphology
 
-$GRADLE -Ploaddir=/vagrant/hmt-lexicon/ -Pdataset=hmt clean && $GRADLE -Ploaddir=/vagrant/hmt-lexicon -Pdataset=hmt buildStems  && $GRADLE -Ploaddir=/vagrant/hmt-lexicon -Pdataset=hmt fst
+$GRADLE -Pdatadir=/vagrant/hmt-lexicon/ -Pdataset=hmt clean && $GRADLE -Pdatadir=/vagrant/hmt-lexicon -Pdataset=hmt buildStems  && $GRADLE -Pdatadir=/vagrant/hmt-lexicon -Pdataset=hmt fst
 
 cd /vagrant/hmt-lexicon
 
